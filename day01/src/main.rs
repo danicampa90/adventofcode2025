ef363961fa83376a898989324a03eddc19827809
Daniele Campana <git@danicampa.com>
2025-12-02 19:52:52 +0100

    Cleaned up day 01

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,5 +1,3 @@
-use std::{fs::File, thread::current};
-
 use crate::{
     parser::{Rotation, parse_full_list},
     wraparound::WraparoundCounter,
@@ -8,49 +6,7 @@ use crate::{
 mod parser;
 mod wraparound;
 
-fn count_times_stopped_at_0(list: &[Rotation], count_pass_throughs: bool) -> usize {
-    let mut result = 0;
-    let mut current_position = 50;
-    for rotation in list {
-        let mut original_position = current_position;
-
-        current_position += match rotation {
-            Rotation::Left(num) => {
-                //print!("L{}", num);
-                -*num
-            }
-            Rotation::Right(num) => {
-                //print!("R{}", num);
-                *num
-            }
-        };
-
-        while current_position < 0 {
-            current_position += 100;
-            if count_pass_throughs && original_position != 0 {
-                result += 1;
-            }
-            original_position = 1; // :puke:
-        }
-        while current_position >= 100 {
-            current_position -= 100;
-            if count_pass_throughs && original_position != 0 {
-                result += 1;
-            }
-            original_position = 1; // :puke:
-        }
-        //print!("=> {}", current_position);
-        if current_position == 0 {
-            //println!("<ZERO!>");
-            result += 1;
-        }
-        //println!();
-    }
-    result
-}
-
 fn count_zeroes(list: &[Rotation]) -> (i32, i32) {
-    let mut result = 0;
     let mut current_position = WraparoundCounter::new(50, 0, 0, 99);
     for rotation in list {
         current_position.add(match rotation {
