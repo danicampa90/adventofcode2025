57c3223230a7dd9c8eea1472515372662a0c0abd
Daniele Campana <git@danicampa.com>
2025-12-22 23:50:12 +0100

    day07 - part2

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,4 +1,7 @@
-use std::{collections::HashSet, mem};
+use std::{
+    collections::{HashMap, HashSet},
+    mem,
+};
 
 use libutils::map2d::{Map2D, map_from_lines};
 
@@ -15,23 +18,34 @@ fn main() {
     let start_coordinate = start_coordinates.first().unwrap();
     println!("Start coordinate: {:?}", start_coordinate);
     ////////////////////
-    let mut frontier = HashSet::new();
-    frontier.insert(*start_coordinate);
+    let mut frontier = HashMap::new();
+    frontier.insert(*start_coordinate, 1);
 
     let mut number_of_splits = 0;
 
     for _steps in 0..map.size_y() {
-        let mut old_frontier = HashSet::new();
+        let mut old_frontier = HashMap::new();
         mem::swap(&mut frontier, &mut old_frontier);
-        for (f_x, f_y) in old_frontier {
+        for ((f_x, f_y), timelines) in old_frontier {
             if map.get_value(f_x, f_y + 1) == '^' {
-                frontier.insert((f_x - 1, f_y + 1));
-                frontier.insert((f_x + 1, f_y + 1));
+                frontier
+                    .entry((f_x - 1, f_y + 1))
+                    .and_modify(|t| *t += timelines)
+                    .or_insert(timelines);
+                frontier
+                    .entry((f_x + 1, f_y + 1))
+                    .and_modify(|t| *t += timelines)
+                    .or_insert(timelines);
                 number_of_splits += 1;
             } else {
-                frontier.insert((f_x, f_y + 1));
+                frontier
+                    .entry((f_x, f_y + 1))
+                    .and_modify(|t| *t += timelines)
+                    .or_insert(timelines);
             }
         }
     }
-    println!("number_of_splits {}", number_of_splits);
+    let number_of_timelines: i64 = frontier.into_iter().map(|(k, v)| v).sum();
+    println!("number of splits {}", number_of_splits);
+    println!("number of timelines {}", number_of_timelines);
 }
